b77705422c404ce71d3ee86d86fa8a87f697f64c
huanghanlin <huanghanlin@onedt.cn>
2014-11-13 13:34:35 +0800

    sss

--- Conf/config.php
+++ Conf/config.php
@@ -2,13 +2,12 @@
 return array(
 	//'配置项'=>'配置值'
 	'APP_NAME'		=>	'app',
-	'__PUBLIC__'    =>  __ROOT__.'/322Public',// 站点公共目录
 //Database
 'DB_TYPE'   		=> 	'mysql', 	// 数据库类型
 'DB_HOST'   		=> 	'localhost',// 服务器地址
 'DB_NAME'   		=> 	'commlib', 	// 数据库名
 'DB_USER'   		=> 	'root', 	// 用户名
-'DB_PWD'    		=> 	'hanlin', 	// 密码
+'DB_PWD'    		=> 	'mysql', 	// 密码
 'DB_PORT'   		=> 	3306, 		// 端口
 'DB_PREFIX' 		=> 	'', 		// 数据库表前缀
 'DB_DEPLOY_TYPE'	=>	0,			// 数据库部署方式 0 集中式 1 分布式	0
